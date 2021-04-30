c7c22738c18877c74e393b47972b50d0f0e1f2e3
James Gale <james@Jamess-Air.attlocal.net>
2021-04-30 19:19:05 -0400

    db create

--- db/init_db.js
+++ db/init_db.js
@@ -52,10 +52,10 @@ async function rebuildDB() {
 
 async function testDB() {
   try {
-    // await dropTables();
-    // await createTables();
-    const id = await getClientById(830515);
-    const all = await getAllClients();
+    await dropTables();
+    await createTables();
+    // const id = await getClientById(830515);
+    // const all = await getAllClients();
 
     console.log(id);
     console.log(all);
